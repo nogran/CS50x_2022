4c6389ab55fffc4027cb5c74d39423f2330fb260
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-02-26 19:00:40 -0300

    hello and mario less projects

---
+++ week_01_c/final_projects/mario-less/mario.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void	ft_print_space(int n)
+{
+	while(n > 0)
+	{
+		printf(" ");
+		n--;
+	}
+}
+
+int	main(void)
+{
+	int	n;
+	int	j;
+	int count_n;
+
+	j = -1;
+	count_n = 1;
+	n = get_int("Weight: ");
+	while(n > 0 && n <= 8)
+	{
+		ft_print_space(n - 1);
+		while(j != 0)
+		{
+			printf("#");
+			j++;
+		}
+		printf("\n");
+		count_n++;
+		j = count_n * -1;
+		n--;
+	}
+}
