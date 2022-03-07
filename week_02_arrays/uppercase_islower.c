6fc2394a8a010e66ea21b27066aaeb38d5a02660
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-03-06 22:49:11 -0300

    islower

---
+++ week_02_arrays/uppercase_islower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	string	s;
+	int	i;
+	int	n;
+
+	s = get_string("Before: ");
+	printf("After: ");
+	for (i = 0, n = strlen(s); i < n; i++)
+	{
+		if (islower(s[i]))
+		{
+			printf("%c", s[i] - 32);
+		}
+		else
+		{
+			printf("%c", s[i]);
+		}
+	}
+	printf("\n");
+}
